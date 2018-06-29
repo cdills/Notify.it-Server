11e4e542a9efc7145cf357379c38ed75ffdd9fe9
Cody Dills <dillscody@gmail.com>
2018-06-29 10:34:14 -0400

    initial commit

---
+++ notifyAPI.py
@@ -0,0 +1,131 @@
+"Watches Reddit submission stream and notifies based on user criteria"
+
+import sys
+import sqlite3
+from multiprocessing import Process
+
+from flask import Flask, g, request
+from flask_restful import Resource, Api, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+masterList = []
+database = "/home/dillscody/rNotify/users.db"   #Will create if noexist
+
+class userprefs(): #Define empty string for argument objects. Not sure if required.
+    sub = ""
+    query = ""
+    udid = ""
+
+
+class CreateMonitor(Resource): #Updates DB with arguments from POST. Returns posted arguments
+    def post(self):
+        try:
+            global args
+            global p1
+            # Parse the arguments
+            parser = reqparse.RequestParser()
+
+            parser.add_argument('sub', type=str, help='subreddit to search')
+            parser.add_argument('query', type=str, help='text to search for')
+            parser.add_argument('udid', type=str, help='unique device ID')
+            args = parser.parse_args()
+
+            newUser = userprefs()
+            newUser.sub = args['sub']
+            newUser.query = args['query']
+            newUser.udid = args['udid']
+            masterList.append(newUser)
+
+            with sqlite3.connect(database) as con:
+                cur = con.cursor()
+                cur.execute(f"INSERT INTO users (id,sub,query,udid) VALUES (Null, \"{newUser.sub}\", \"{newUser.query}\", \"{newUser.udid}\") WHERE NOT EXISTS (SELECT * FROM users WHERE sub = \"{newUser.sub}\" AND query = \"{newUser.query}\" AND udid = \"{newUser.udid}\")")
+                con.commit()
+
+            return {'sub': args['sub'], 'query': args['query'], 'udid': args['udid']}
+
+        except Exception as e:
+            return {'error': str(e)}
+
+class ClearSearch(Resource): #Removes entry from DB for matching user ID from POST. Returns user ID
+    def post(self):
+        try:
+            global args
+            global p1
+            # Parse the arguments
+            parser = reqparse.RequestParser()
+
+            parser.add_argument('udid', type=str, help='unique device ID')
+            args = parser.parse_args()
+
+            newUser = userprefs()
+
+            newUser.udid = args['udid']
+
+
+            with sqlite3.connect(database) as con:
+                cur = con.cursor()
+                cur.execute(f"DELETE FROM users WHERE udid = \"{newUser.udid}\"") #Will delete ALL matching entries.
+                con.commit()
+
+            return {'udid': args['udid']}
+
+        except Exception as e:
+            return {'error': str(e)}
+
+api.add_resource(ClearSearch, '/ClearSearch') #Adds the functions to the specified URL for API
+api.add_resource(CreateMonitor, '/CreateMonitor') #Adds the functions to the specified URL for API
+
+def create_connection(db_file): #Creates DB connection, returns connection object
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Exception as e:
+        print(e)
+
+    return None
+
+def create_table(conn, create_table_sql): #Creates table with specified query and connection object.
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Exception as e:
+        print(e)
+
+###def main():
+#    p1 = Process(app.run(debug=True))
+ #   p1.start()"""
+
+
+def main():
+
+
+    sql_create_user_table = """ CREATE TABLE IF NOT EXISTS users (
+                                        id integer PRIMARY KEY,
+                                        sub text NOT NULL,
+                                        query text,
+                                        UDID text
+                                    ); """
+
+
+    # create a database connection
+    conn = create_connection(database)
+    if conn is not None:
+        # create user table
+        create_table(conn, sql_create_user_table)
+
+    else:
+        print("Error! cannot create the database connection.")
+
+    app.run(host='0.0.0.0', port = 5500) #Starts the API listening on localhost:5000
+
+
+#for item in masterList:
+ #   sub = item.sub
+  #  query = item.query
+
+
+# gui()
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ rnotifyParser.py
@@ -0,0 +1,128 @@
+import praw
+import sqlite3
+import concurrent.futures
+import requests
+import json
+import threading
+import time
+import logging
+import socket
+import sys
+
+
+database = "/home/dillscody/rNotify/users.db"
+lock_socket = None
+
+def lock_socket():
+    global lock_socket
+    lock_socket = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+    lock_id = "dillscody.parser"   # this should be unique. using your username as a prefix is a convention
+    lock_socket.bind('\0' + lock_id)
+    logging.debug("Acquired lock %r" % (lock_id,))
+    return True
+
+class RepeatedTimer(object):
+  def __init__(self, interval, function, *args, **kwargs):
+    self._timer = None
+    self.interval = interval
+    self.function = function
+    self.args = args
+    self.kwargs = kwargs
+    self.is_running = False
+    self.next_call = time.time()
+    self.start()
+
+  def _run(self):
+    self.is_running = False
+    self.start()
+    self.function(*self.args, **self.kwargs)
+
+  def start(self):
+    if not self.is_running:
+      self.next_call += self.interval
+      self._timer = threading.Timer(self.next_call - time.time(), self._run)
+      self._timer.start()
+      self.is_running = True
+
+  def stop(self):
+    self._timer.cancel()
+    self.is_running = False
+
+def counter():
+    count = 0
+    count += 1
+    print(count)
+
+def retrieveUsers():
+    global userList
+    con = sqlite3.connect(database, check_same_thread=False)
+    cur = con.cursor()
+    cur.execute("SELECT DISTINCT ID, sub, query, udid FROM users Group BY sub, query, udid ORDER BY id")
+    userList = cur.fetchall()
+    con.close()
+    return userList
+
+
+def hello(name):
+    print("Hello %s!" % name)
+
+
+def getReddit():
+    global reddit, subreddit
+    reddit = praw.Reddit(client_id='', client_secret="l",
+                         password='stupid', user_agent='',
+                         username='')
+    subreddit = reddit.subreddit("all")
+
+    # http://127.0.0.1:65010/authorize_callback
+    return subreddit
+
+
+def createAllStream(subreddit):  # must be run with reddit_auth as argument
+
+    stream = subreddit.stream.submissions()
+    return stream
+
+
+def parseSubmission(submission):
+    userList = retrieveUsers()
+    subTitle_normal = submission.title.lower()
+    display = submission.subreddit.display_name.lower()
+    for user in userList:
+        search_str = user[2].lower()
+        if user[1].lower() == display and search_str in subTitle_normal:
+            print("Sending Notifcation", submission.title, user[3])
+            postNotif(submission.title, submission.shortlink, user[3])
+
+
+def postNotif(title, shortlink,  userUUID):
+    url = "https://onesignal.com/api/v1/notifications"
+    data = {"app_id": "",
+            "filters": [
+                {"field": "tag", "key": "uuid", "relation": "=", "value": userUUID}
+                ],
+            "contents": {"en": shortlink},
+            "headings": {"en": title},
+            "data": {"title": title, "body": shortlink},
+            "mutable_content": "true"}
+    data_json = json.dumps(data)
+    headers = {"Content-type": "application/json; charset=utf-8",
+               "Authorization": "Basic "}
+    r = requests.post(url, data=data_json, headers=headers)
+    print(r.status_code, r.reason, r.text)
+
+
+
+def main():
+    global userList
+    lock_socket()
+    stream = createAllStream(getReddit())
+   # for submission in stream:
+      #  parseSubmission(submission)
+    executor = concurrent.futures.ProcessPoolExecutor(3)
+    futures = [executor.submit(parseSubmission, submission) for submission in stream]
+    concurrent.futures.as_completed(futures)
+
+
+if __name__ == "__main__":
+    main()
