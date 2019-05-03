1c13d899fb23200c015de6ab76b2c246869f2655
Cody Dills <dillscody@gmail.com>
2019-05-03 10:50:31 -0400

    begin stagin api endpoings

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  async function getUsers() {
+    db = await dbPromise;
+    return users = await db.get( "SELECT DISTINCT ID, sub, query, udid FROM users Group BY sub, query, udid ORDER BY id");
+  }
+  res.send(getUsers());
 });
 
 module.exports = router;
